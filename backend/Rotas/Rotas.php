0e42b00fa08baa4c57a7812020c886ab2e3694f3
Gregz747 <gregory.lvgg1212@gmail.com>
2025-10-23 11:32:03 -0300

    trazendo tudo pra ca

--- backend/Rotas/Rotas.php
+++ backend/Rotas/Rotas.php
@@ -47,6 +47,27 @@ class Rotas
                  '/login' => 'AuthController@login',
                  '/logout' => 'AuthController@logout',
                  '/admin/dashboard' => 'Admin\DashboardController@index',
+              //Itens pedidos
+                   "/itenspedidos" => "ItensPedidosController@index",
+                  "/itenspedidos/criar" => "ItensPedidosController@viewCriarItemPedido",
+                  "/itenspedidos/listar" => "ItensPedidosController@viewListarItemPedido",
+                  "/itenspedidos/editar/{id}" => "ItensPedidosController@viewEditarItemPedido",
+                  "/itenspedidos/excluir/{id}" => "ItensPedidosController@viewExcluirItemPedido",
+                  "/itenspedidos/{id}/relatorio/{data1}/{data2}" => "ItensPedidosController@relatorioitenspedidos",
+               //Pedidos
+                  "/pedido" => "PedidosController@index",
+                  "/pedido/criar" => "PedidosController@viewCriarPedido",
+                  "/pedido/listar" => "PedidosController@viewListarPedido",
+                  "/pedido/editar/{id}" => "PedidosController@viewEditarPedido",
+                  "/pedido/excluir/{id}" => "PedidosController@viewExcluirPedido",
+                     "/pedido/{id}/relatorio/{data1}/{data2}" => "Pedidos@relatorioPedido",
+               //Produtos
+                  "/produto" => "ProdutosController@index",
+                  "/produto/criar" => "ProdutosController@viewCriarProdutos",
+                  "/produto/listar" => "ProdutosController@viewListarProdutos",
+                  "/produto/editar/{id}" => "ProdutosController@viewEditarProduto",
+                  "/produto/excluir/{id}" => "ProdutosController@viewExcluirProdutos",
+                  "/produto/{id}/relatorio/{data1}/{data2}" => "ProdutosController@relatorioProduto",
           ],
 
         "POST" => [
@@ -73,6 +94,18 @@ class Rotas
                 // Login
                 '/register' => 'AuthController@cadastrarUsuario',
                 '/login' => 'AuthController@authenticar',
+                //itens pedidos
+               "/itenspedidos/salvar" => "ItensPedidosController@salvarItemPedido",
+               "/itenspedidos/atualizar/{id}" => "ItensPedidosController@atualizarItemPedido",
+               "/itenspedidos/deletar/{id}" => "ItensPedidosController@deletarItemPedido",
+               //Pedidos
+                  "/pedido/salvar" => "PedidosController@salvarPedido",
+               "/pedido/atualizar/{id}" => "PedidosController@atualizarPedido",
+               "/pedido/deletar/{id}" => "PedidosController@deletarPedido",
+               //produtos
+               "/produto/salvar" => "ProdutosController@salvarProduto",
+               "/produto/atualizar/{id}" => "ProdutosController@atualizarProduto",
+               "/produto/deletar/{id}" => "ProdutosController@deletarProduto",
                 
             ]
         ];
