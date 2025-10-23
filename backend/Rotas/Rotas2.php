5e06cc16dc5420809883a33ecfa6622f22cc0df4
Gregz747 <gregory.lvgg1212@gmail.com>
2025-10-23 11:32:35 -0300

    trazendo tudo pra ca

---
+++ backend/Rotas/Rotas2.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Koketsu\Rotas;
+
+class Rotas
+{
+    public static function get()
+    {
+        var_dump("teste");
+        return [ 
+             "GET" => [
+        // o caminho da URL    o nome do controlle e o metodo do controle 
+        //usuarios
+        "/usuarios" => "UsuarioController@index",
+        "/usuario/criar" => "UsuarioController@viewCriarUsuarios",
+        "/usuario/listar/{pagina}" => "UsuarioController@viewListarUsuarios",
+        "/usuario/editar/{id}" => "UsuarioController@viewEditarUsuarios",
+        "/usuario/excluir/{id}" => "UsuarioController@viewExcluirUsuarios",
+        "/usuario/{id}/relatorio/{data1}/{data2}" => "UsuarioController@relatorioUsuario",
+        '/register' => 'AuthController@register',
+        '/login' => 'AuthController@login',
+        '/logout' => 'AuthController@logout',
+        '/admin/dashboard' => 'Admin\DashboardController@index',
+
+        //Itens pedidos
+        "/itenspedidos" => "ItensPedidosController@index",
+        "/itenspedidos/criar" => "ItensPedidosController@viewCriarItemPedido",
+        "/itenspedidos/listar" => "ItensPedidosController@viewListarItemPedido",
+        "/itenspedidos/editar/{id}" => "ItensPedidosController@viewEditarItemPedido",
+        "/itenspedidos/excluir/{id}" => "ItensPedidosController@viewExcluirItemPedido",
+        "/itenspedidos/{id}/relatorio/{data1}/{data2}" => "ItensPedidosController@relatorioitenspedidos",
+        //Pedidos
+        "/pedido" => "PedidosController@index",
+        "/pedido/criar" => "PedidosController@viewCriarPedido",
+        "/pedido/listar" => "PedidosController@viewListarPedido",
+        "/pedido/editar/{id}" => "PedidosController@viewEditarPedido",
+        "/pedido/excluir/{id}" => "PedidosController@viewExcluirPedido",
+         "/pedido/{id}/relatorio/{data1}/{data2}" => "Pedidos@relatorioPedido",
+        //Produtos
+        "/produto" => "ProdutosController@index",
+        "/produto/criar" => "ProdutosController@viewCriarProdutos",
+        "/produto/listar" => "ProdutosController@viewListarProdutos",
+        "/produto/editar/{id}" => "ProdutosController@viewEditarProduto",
+        "/produto/excluir/{id}" => "ProdutosController@viewExcluirProdutos",
+        "/produto/{id}/relatorio/{data1}/{data2}" => "ProdutosController@relatorioProduto",
+    ],
+    "POST" => [
+        //usuarios
+        "/usuario/salvar" => "UsuarioController@salvarUsuario",
+       "/usuario/atualizar/{id}" => "UsuarioController@atualizarUsuario",
+       "/usuario/deletar/{id}" => "UsuarioController@deletarUsuario",
+        '/register' => 'AuthController@cadastrarUsuario',
+       '/login' => 'AuthController@authenticar',
+        //itens pedidos
+       "/itenspedidos/salvar" => "ItensPedidosController@salvarItemPedido",
+       "/itenspedidos/atualizar/{id}" => "ItensPedidosController@atualizarItemPedido",
+       "/itenspedidos/deletar/{id}" => "ItensPedidosController@deletarItemPedido",
+       //Pedidos
+         "/pedido/salvar" => "PedidosController@salvarPedido",
+       "/pedido/atualizar/{id}" => "PedidosController@atualizarPedido",
+       "/pedido/deletar/{id}" => "PedidosController@deletarPedido",
+       //produtos
+        "/produto/salvar" => "ProdutosController@salvarProduto",
+       "/produto/atualizar/{id}" => "ProdutosController@atualizarProduto",
+       "/produto/deletar/{id}" => "ProdutosController@deletarProduto",
+
+            ]
+        ];
+    }
+}
