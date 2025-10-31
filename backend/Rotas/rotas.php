eed0a621bc61d75fe625235410bb59492dfa670f
thurflx213 <thurfelix10@gmail.com>
2025-10-31 11:22:31 -0300

    feat: fewfe

--- backend/Rotas/rotas.php
+++ backend/Rotas/rotas.php
@@ -14,8 +14,7 @@ class Rotas
                  "/usuario/listar" => "UsuarioController@viewListarUsuarios",
                  "/usuario/editar/{id}" => "UsuarioController@viewEditarUsuarios",
                  "/usuario/excluir/{id}" => "UsuarioController@viewExcluirUsuarios",
-                  "/usuario/ativar/{id}" => "UsuarioController@viewAtivarUsuarios",
-                 "/usuario/relatorios" => "UsuarioController@viewListarUsuarios",
+                 "/usuario/{id}/relatorio/{data1}/{data2}" => "UsuarioController@relatorioUsuario",
               // Categorias
                  "/categorias"        => "CategoriasController@index",
                  "/categoria/criar"   => "CategoriasController@viewCriarCategoria",
@@ -33,7 +32,7 @@ class Rotas
               // Perfil
                  "/perfis"        => "PerfilController@index",
                  "/perfil/criar"  => "PerfilController@viewCriarPerfil",
-                 "/perfil/listar" => "PerfilController@viewListarPerfis",
+                 "/perfil/listar/{pagina}" => "PerfilController@viewListarPerfis",
                  "/perfil/editar/{id}" => "PerfilController@viewEditarPerfil",
                  "/perfil/excluir/{id}" => "PerfilController@viewExcluirPerfil",
               // Tamanhos
@@ -42,56 +41,8 @@ class Rotas
                  "/tamanho/listar"  => "TamanhoController@viewListarTamanhos",
                  "/tamanho/editar/{id}"  => "TamanhoController@viewEditarTamanho",
                  "/tamanho/excluir/{id}" => "TamanhoController@viewExcluirTamanho",
-              // Itens Pedidos   
-                 "/itenspedidos" => "ItensPedidosController@index",
-                 "/itenspedidos/{id}" => "ItensPedidosController@viewItemPedidoUnico",
-                 "/itenspedidos/criar" => "ItensPedidosController@viewCriarItemPedido",
-                 "/itenspedidos/listar/{pagina}" => "ItensPedidosController@viewListarItemPedido",
-                 "/itenspedidos/editar/{id}" => "ItensPedidosController@viewEditarItemPedido",
-                 "/itenspedidos/excluir/{id}" => "ItensPedidosController@viewExcluirItemPedido",
-                 "/itenspedidos/{id}/relatorio/{data1}/{data2}" => "ItensPedidosController@relatorioitenspedidos",
-              // Pedidos
-                 "/pedido" => "PedidosController@index",
-                 "/pedido/{id}" => "PedidosController@viewPedidoUnico",
-                 "/pedido/criar" => "PedidosController@viewCriarPedido",
-                 "/pedido/listar/{pagina}" => "PedidosController@viewListarPedido",
-                 "/pedido/editar/{id}" => "PedidosController@viewEditarPedido",
-                 "/pedido/excluir/{id}" => "PedidosController@viewExcluirPedido",
-                 "/pedido/{id}/relatorio/{data1}/{data2}" => "PedidosController@relatorioPedido",
-              //Produtos
-                 "/produtos/listar" => "ProdutosController@viewListarProduto",
-                 "/produtos/criar" => "ProdutosController@viewCriarProduto",
-                  '/api/produtos' => 'PublicApiController@getProdutos',
-                 "/produtos/listar/{pagina}" => "ProdutosController@viewlistarProduto",
-                 "/produtos/editar/{id}" => "ProdutosController@viewEditarProdutos",
-                 "/produtos/excluir/{id}" => "ProdutosController@viewExcluirProduto",
-                 "/produtos/ativar/{id}" => "ProdutosController@viewAtivarProdutos",
-                 "/produtos/{id}/relatorio/{data1}/{data2}" => "ProdutosController@relatorioProduto",
-               //Avaliação
-                 "/backend/avaliacao" => "AvaliacaoController@index",
-                 "/backend/avaliacao/criar" => "AvaliacaoController@viewCriarAvaliacoes",
-                 "/backend/avaliacao/listar" => "AvaliacaoController@viewListarAvaliacoes",
-                 "/backend/avaliacao/editar/{id}" => "AvaliacaoController@viewEditarAvaliacoes",
-                 "/backend/avaliacao/excluir/{id}" => "AvaliacaoController@viewExcluirAvaliacoes",
-              //Carrinho
-                 "/backend/carrinho" => "CarrinhoController@index",
-                 "/backend/carrinho/criar" => "CarrinhoController@viewCriarCarrinho",
-                 "/backend/carrinho/listar" => "CarrinhoController@viewListarCarrinho",
-                 "/backend/carrinho/editar/{id}" => "CarrinhoController@viewEditarCarrinho",
-                 "/backend/carrinho/excluir/{id}" => "CarrinhoController@viewExcluirCarrinho",
-              //Estoque_Movimentação
-                 "/backend/EstoqueMovimentacao" => "EstoqueController@index",
-                 "/backend/EstoqueMovimentacao/criar" => "EstoqueController@viewCriarEstoque_Movimentacao",
-                 "/backend/EstoqueMovimentacao/listar" => "EstoqueController@viewListarEstoque_Movimentacao",
-                 "/backend/EstoqueMovimentacao/editar/{id}" => "EstoqueController@viewEditarEstoque_Movimentacao",
-                 "/backend/EstoqueMovimentacao/excluir/{id}" => "EstoqueController@viewExcluirEstoque_Movimentacao",
-             //Imagens
-                 "/backend/Imagens" => "ImagensController@index",
-                 "/backend/Imagens/criar" => "ImagensController@viewCriarImagem",
-                 "/backend/Imagens/listar" => "ImagensController@viewListarImagem",
-                 "/backend/Imagens/editar/{id}" => "ImagensController@viewEditarImagem",
-                 "/backend/Imagens/excluir/{id}" => "ImagensController@viewExcluirImagem",
-             // Login
+
+              // Login
                  '/register' => 'AuthController@register',
                  '/login' => 'AuthController@login',
                  '/logout' => 'AuthController@logout',
@@ -99,12 +50,10 @@ class Rotas
           ],
 
         "POST" => [
-               '/api/pedidos' => 'PublicApiController@salvarPedido',
                 // Usuarios 
                 "/usuario/salvar" => "UsuarioController@salvarUsuario",
-                "/usuario/atualizar" => "UsuarioController@atualizarUsuario",
-                "/usuario/deletar" => "UsuarioController@deletarUsuario",
-                "/usuario/ativar" => "UsuarioController@ativarUsuario",
+                "/usuario/atualizar/{id}" => "UsuarioController@atualizarUsuario",
+                "/usuario/deletar/{id}" => "UsuarioController@deletarUsuario",
                 // Categorias
                 "/categoria/salvar"    => "CategoriasController@salvarCategoria",
                 "/categoria/atualizar/{id}" => "CategoriasController@atualizarCategoria",
@@ -119,39 +68,8 @@ class Rotas
                 "/perfil/deletar/{id}"   => "PerfilController@deletarPerfil",
                 // Tamanhos
                 "/tamanho/salvar"    => "TamanhoController@salvarTamanho",
-                "/tamanho/atualizar" => "TamanhoController@atualizarTamanho",
+                "/tamanho/atualizar/{id}" => "TamanhoController@atualizarTamanho",
                 "/tamanho/deletar/{id}"   => "TamanhoController@deletarTamanho",
-                //itens pedidos
-                "/itenspedidos/salvar" => "ItensPedidosController@salvarItemPedido",
-                "/itenspedidos/atualizar/{id}" => "ItensPedidosController@atualizarItemPedido",
-                "/itenspedidos/deletar/{id}" => "ItensPedidosController@deletarItemPedido",
-                //Pedidos
-                "/pedido/salvar" => "PedidosController@salvarPedido",
-                "/pedido/atualizar/{id}" => "PedidosController@atualizarPedido",
-                "/pedido/deletar/{id}" => "PedidosController@deletarPedido",
-                //produtos
-                "/produtos/salvar" => "ProdutosController@salvarProduto",
-                "/produtos/atualizar" => "ProdutosController@atualizarProdutos",
-                "/produtos/deletar" => "ProdutosController@deletarProdutos",
-                "/produtos/ativar" => "produtosController@ativarProdutos",
-                //avaliacao
-                "/backend/avaliacao/salvar" => "AvaliacaoController@salvarAvaliacao",
-                "/backend/avaliacao/atualizar/{id}" => "AvaliacaoController@atualizarAvaliacao",
-                "/backend/avaliacao/deletar/{id}" => "AvaliacaoController@deletarAvaliacao",
-                //carrinho
-                "/backend/carrinho/salvar" => "CarrinhoController@salvarCarrinho",
-                "/backend/carrinho/atualizar/{id}" => "CarrinhoController@atualizarCarrinho",                
-                "/backend/carrinho/deletar/{id}" => "CarrinhoController@deletarCarrinho",
-                //EstoqueMovimentacao
-                "/backend/EstoqueMovimentacao/salvar" => "EstoqueController@salvarEstoque_Movimentacao",
-                "/backend/EstoqueMovimentacao/atualizar/{id}" => "EstoqueController@atualizarEstoque_Movimentacao",
-                "/backend/EstoqueMovimentacao/deletar/{id}" => "EstoqueController@deletarEstoque_Movimentacao",
-                //imagens
-                "/backend/Imagem/salvar" => "ImagensController@salvarImagens",
-                "/backend/Imagem/atualizar/{id}" => "ImagensController@atualizarImagens",
-                "/backend/Imagem/deletar/{id}" => "ImagensController@deletarImagens",
-                
-
                 // Login
                 '/register' => 'AuthController@cadastrarUsuario',
                 '/login' => 'AuthController@authenticar',
